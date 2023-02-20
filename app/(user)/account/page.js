6efe51bc74b2d06dc69e3c391c718213f9f8d678
anxhirr <anxhirr@gmail.com>
2023-02-20 02:13:06 +0100

    small change just to trigger auto deployment

--- app/(user)/account/page.js
+++ app/(user)/account/page.js
@@ -1,12 +1,13 @@
 'use client'
 
 import { signOut } from 'next-auth/react'
+import { AlternativeBtn } from '../../../components/buttons'
 
 const Account = () => {
   return (
     <div>
       Account
-      <button onClick={() => signOut()}>Dilni</button>
+      <AlternativeBtn onClick={() => signOut()}>Dilni</AlternativeBtn>
     </div>
   )
 }
