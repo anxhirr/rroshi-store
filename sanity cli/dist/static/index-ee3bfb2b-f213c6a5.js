3963dbbcf60670ebbaea3f2becdcfcb92435d7e0
anxhirr <anxhirr@gmail.com>
2023-02-22 17:01:36 +0100

    sanity cli folder

---
+++ sanity cli/dist/static/index-ee3bfb2b-f213c6a5.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-def00177-7a40b786.js";import"./sanity-00adb327.js";import"./index-09960450.js";export{m as DocumentPaneProvider,a as default};
