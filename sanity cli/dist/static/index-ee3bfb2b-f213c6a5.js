eb60843f79a8ead0455ef2706af60da60e9ea105
anxhirr <anxhirr@gmail.com>
2023-02-24 11:29:13 +0100

    Reverted "sanity cli folder" commit

--- sanity cli/dist/static/index-ee3bfb2b-f213c6a5.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPaneProvider as m,DocumentPane as a}from"./index-def00177-7a40b786.js";import"./sanity-00adb327.js";import"./index-09960450.js";export{m as DocumentPaneProvider,a as default};
